e46e2d210ca05bee913d524456594665d65c1ccb
Juan Negro <jnegro@itba.edu.ar>
2023-04-10 20:06:17 -0300

    Add get_collision_time for Ball

--- src/collision.rs
+++ src/collision.rs
@@ -30,7 +30,22 @@ impl Collide<Ball> for Ball {
     }
 
     fn get_collision_time(&self, other: &Ball) -> f64 {
-        todo!()
+        let delta_coords = (self.x - other.x, self.y - other.y);
+        let delta_vel = (self.v_x - other.v_x, self.v_y - other.v_y);
+
+        let coords_dot_vel = delta_coords.0 * delta_vel.0 + delta_coords.1 * delta_vel.1;
+        if coords_dot_vel >= 0.0 {
+            return std::f64::INFINITY;
+        }
+        let sigma = self.r + other.r;
+        let coords_dot_coords = delta_coords.0 * delta_coords.0 + delta_coords.1 * delta_coords.1;
+        let vel_dot_vel = delta_vel.0 * delta_vel.0 + delta_vel.1 * delta_vel.1;
+        let discriminant =
+            coords_dot_vel * coords_dot_vel - vel_dot_vel * (coords_dot_coords - sigma * sigma);
+        if discriminant < 0.0 {
+            return std::f64::INFINITY;
+        }
+        (-coords_dot_vel - discriminant.sqrt()) / vel_dot_vel
     }
 }
 
