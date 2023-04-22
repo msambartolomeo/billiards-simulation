f136a356aa0b2408923c6e8dd72c2dfb5fc6dc1e
Juan Negro <jnegro@itba.edu.ar>
2023-04-21 22:46:11 -0300

    fix starting position values

--- analysis/run_diff_starting_pos.py
+++ analysis/run_diff_starting_pos.py
@@ -3,25 +3,29 @@ import subprocess
 
 import numpy as np
 
-Y_MAX_OFFSET = 0.0
-Y_MIN_OFFSET = -6.0
-Y_STEP = 0.6
+Y_MAX_POS = 0.56
+Y_MIN_POS = 0.42
+RUNS_PER_OFFSET = 10
+Y_STEP = 0.014
+
 
 RUNS_PER_POS = 50
-X_STARTING_POS = 0.56
-
-RESULTS_PATH = f"./analysis/results/diff_starting_pos/offset{{}}/"
 
 
-for y_offset in np.arange(Y_MIN_OFFSET, Y_MAX_OFFSET + Y_STEP, Y_STEP):
-    os.makedirs(RESULTS_PATH.format(round(y_offset, 2)), exist_ok=True)
+RESULTS_PATH = f"./analysis/results/diff_starting_pos/pos{{}}/"
+
+
+for y_pos in np.arange(Y_MIN_POS, Y_MAX_POS, Y_STEP):
+    y_pos_str = str(round(y_pos, 3))
+    y_offset_str = str(round(Y_MAX_POS - y_pos, 3))
+    print(f"Starting runs for Y position {y_pos_str} and offset {y_offset_str}")
+    os.makedirs(RESULTS_PATH.format(y_pos_str), exist_ok=True)
     for run_idx in range(RUNS_PER_POS):
         subprocess.run(
             [
                 "./target/release/billiards-simulation",
-                "-w" + str(round(y_offset, 2)),
+                "-w" + y_offset_str,
                 "-t",
-                RESULTS_PATH.format(round(y_offset, 2))
-                + f"offset{round(y_offset, 2)}_run{run_idx}.txt",
+                RESULTS_PATH.format(y_pos_str) + f"pos{y_pos_str}_run{run_idx}.txt",
             ]
         )
